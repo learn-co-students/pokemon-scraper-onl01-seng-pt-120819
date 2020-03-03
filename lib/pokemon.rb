ee77553535600efc8b2393748c69dc015a32253b
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-03-03 05:03:43 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,21 @@
 class Pokemon
+
+  attr_accessor :name, :type, :db
+  attr_reader :id
+
+  def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(name, type, db)
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type)
+      VALUES (?, ?), name, type
+    SQL
+
+    @db[:conn].execute(sql, self.name, self.type)
+  end
 end
